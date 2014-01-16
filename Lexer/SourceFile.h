a53233e56b9f3f012e3f622e10dd7cb873f0ddbb
unknown <huiyang@huiyang-win81.fareast.corp.microsoft.com>
2014-01-16 16:27:06 +0800

    start to work on lexer;

---
+++ Lexer/SourceFile.h
@@ -0,0 +1,6 @@
+#ifndef __SOURCE_FILE__
+#define __SOURCE_FILE__
+
+/// implement SourceFile, which loads and reads file, fetches tokens from file;
+
+#endif
---
+++ Lexer/Token.h
@@ -0,0 +1,12 @@
+#ifndef __TOKEN__
+#define __TOKEN__
+
+/// represent a Token got from a SourceFile;
+class Token
+{
+public:
+	Token(void);
+	~Token(void);
+};
+
+#endif
